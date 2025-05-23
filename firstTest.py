594d25fbf881b905f754de36118ce732b814744c
adhum <113613114+0adhum0@users.noreply.github.com>
2025-05-23 14:54:23 +0200

    .

--- firstTest.py
+++ firstTest.py
@@ -1,22 +1,20 @@
 from dotenv import load_dotenv
 import os
+import lyricsgenius
 
-load_dotenv()  # Load variables from .env file
-
+load_dotenv()
 token = os.environ.get("GENIUS_API_TOKEN")
 
 if not token:
     print("Error: GENIUS_API_TOKEN not set")
     exit(1)
 
-import lyricsgenius
-
 genius = lyricsgenius.Genius(token)
 
 url = "https://genius.com/Twenty-one-pilots-stressed-out-lyrics"
 if url:
     lyrics = genius.lyrics(song_url=url)
-    print(lyrics.strip("More "))
-
+    clean_lyrics = lyrics.split("More ")[0].strip()  # split at 'More ' and keep the first part
+    print(clean_lyrics)
 else:
     print("Song not found.")
